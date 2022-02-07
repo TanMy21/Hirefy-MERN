19e956992da5888440de670475a2ab66b709d2c6
Tanmay 1 <tanmay.pvp.17@gmail.com>
2022-02-07 20:08:34 +0530

    user model

---
+++ server/models/User.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please provide name"],
+      minlength: 3,
+      maxlength: 20,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: [true, "Please provide email"],
+      validate: {
+        validator: validator.isEmail,
+        message: "Please provide a valid email",
+      },
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Please provide password"],
+      minlength: 4,
+    },
+    location: {
+      type: String,
+      trim: true,
+      maxlength: 20,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
