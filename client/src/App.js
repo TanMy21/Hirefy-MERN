da04c54164d8b3f12e78ee263448698099560fdd
Tanmay 1 <tanmay.pvp.17@gmail.com>
2022-02-09 21:28:13 +0530

    dashboard, nested pages, protected routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,30 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Dashboard, Error, Landing, Register } from "./pages";
+import { Error, Landing, Register, ProtectedRoute } from "./pages";
+import {
+  AllJobs,
+  Addjob,
+  Profile,
+  Stats,
+  SharedLayout,
+} from "./pages/Dashboard";
+
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Dashboard />} />
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <SharedLayout />
+            </ProtectedRoute>
+          }
+        >
+          <Route index element={<Stats />} />
+          <Route path="all-jobs" element={<AllJobs />} />
+          <Route path="add-job" element={<Addjob />} />
+          <Route path="profile" element={<Profile />} />
+        </Route>
         <Route path="/register" element={<Register />} />
         <Route path="/landing" element={<Landing />} />
         <Route path="*" element={<Error />} />
---
+++ client/src/pages/Dashboard/index.js
@@ -0,0 +1,7 @@
+import Addjob from "./AddJob";
+import AllJobs from "./AllJobs";
+import Profile from "./Profile";
+import Stats from "./Stats";
+import SharedLayout from "./SharedLayout";
+
+export { AllJobs, Addjob, Profile, Stats, SharedLayout };
