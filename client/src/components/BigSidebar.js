510b77f639d5eb9c334275cde33b058a31051c9a
Tanmay 1 <tanmay.pvp.17@gmail.com>
2022-02-09 21:43:49 +0530

    shared layout v2 and react icons

---
+++ client/src/components/BigSidebar.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Wrapper from "../assets/wrappers/SmallSidebar";
+
+
+const SmallSidebar = () => {
+    return (
+        <Wrapper>
+          <h4>Big Sidebar</h4>
+        </Wrapper>
+      )
+};
+
+export default SmallSidebar;
